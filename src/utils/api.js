984214da4e25eb807a92df3f77a6632596367aa0
Aluno TripleTen <wesleyalmada@yahoo.com.br>
2024-02-22 02:19:30 -0300

    api criada novamente

---
+++ src/utils/api.js
@@ -0,0 +1,102 @@
+class Api {
+  constructor(config) {
+    this.baseUrl = config.baseUrl;
+    this.headers = config.headers;
+  }
+
+  getUserInfo() {
+    return fetch(`${this.baseUrl}/users/me`, {
+      headers: this.headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Erro: ${res.status}`);
+    });
+  }
+
+  setUserInfo(data) {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this.headers,
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Erro ao editar o perfil: ${res.status}`);
+    });
+  }
+
+  getCards() {
+    return fetch(`${this.baseUrl}/cards`, {
+      headers: this.headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Erro: ${res.status}`);
+    });
+  }
+
+  changeLikeCardStatus(cardId, isLiked) {
+    const method = isLiked ? "PUT" : "DELETE";
+    return fetch(`${this.baseUrl}/cards/likes/${cardId}`, {
+      method: method,
+      headers: this.headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Erro: ${res.status}`);
+    });
+  }
+
+  deleteCard(cardId) {
+    return fetch(`${this.baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this.headers,
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Erro: ${res.status}`);
+    });
+  }
+
+  setUserAvatar(data) {
+    return fetch(`${this.baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this.headers,
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Erro: ${res.status}`);
+    });
+  }
+
+  addCard(data) {
+    return fetch(`${this.baseUrl}/cards`, {
+      method: "POST",
+      headers: this.headers,
+      body: JSON.stringify(data),
+    }).then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      return Promise.reject(`Erro ao adicionar o card: ${res.status}`);
+    });
+  }
+}
+
+const apiInstance = new Api({
+  baseUrl: "https://around.nomoreparties.co/v1/web_ptbr_08",
+  headers: {
+    authorization: "7887b144-3999-4d14-a3d6-51691cca960c",
+    "Content-Type": "application/json",
+  },
+});
+
+export default apiInstance;
